869d1423ac2d6f6c790a7bbba359403f551aeea1
Imam Abdul Azis <imamabdulazis028@gmail.com>
2020-04-03 00:10:14 +0700

    first upload

---
+++ src/Container/LoginPage/LoginPage.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './Login.css';
+import { connect } from 'react-redux';
+
+import { userActions } from '../_actions';
+
+class LoginPage extends Component {
+
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.dispatch(userActions.logout());
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        const { dispatch } = this.props;
+        if (username && password) {
+            dispatch(userActions.login(username, password));
+        }
+    }
+
+    render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
+        return (
+            <div className="wrapper fadeInDown">
+                <div id="formContent">
+                    <div className="fadeIn first">
+                        <img src="https://img.icons8.com/clouds/2x/login-rounded-right.png" id="icon" alt="User Icon" />
+                    </div>
+                    <form>
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { loggingIn } = state.authentication;
+    return {
+        loggingIn
+    };
+}
+
+const connectedLoginPage = connect(mapStateToProps)(LoginPage);
+export { connectedLoginPage as LoginPage }; 
